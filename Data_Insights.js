609559569277e76e5e8e366fcb8d8b6f3d023141
Suraksha Balachandra <96290932+sur-aksha@users.noreply.github.com>
2024-02-01 13:18:01 +0100

    New repo test

--- Data_Insights.js
+++ Data_Insights.js
@@ -1,124 +1,240 @@
 (function () {
-   let template = document.createElement("template");
-   template.innerHTML = `
- <style>
-     :host {}
-     /* Style for the container */
-     div {
-         margin: 25px auto;
-         max-width: 80%;
-     }
-     /* Style for the generated text area */
-     #generated-text {
-         padding: 10px 3%;
-         font-size: 16px;
-         border: 1px solid #ccc;
-         border-radius: 5px;
-         width:94%;
-     }
-     /* Style for the button */
-     #insights-button {
-         padding: 10px;
-         font-size: 16px;
-         background-color: #3cb6a9;
-         color: #fff;
-         border: none;
-         border-radius: 5px;
-         cursor: pointer;
-         width: 25%;
-     }
-     /* Style for the input field */
-     #text-input {
-         padding: 10px;
-         font-size: 16px;
-         border: 1px solid #ccc;
-         border-radius: 5px;
-         width: 70%;
-     }
- </style>
-   <div>
-     <h2>Data Insights</h2>
-       <ul>
-           <li>Dummy Text 1</li>
-           <li>Dummy Text 2</li>
-           <li>Dummy Text 3</li>
-       </ul>
-   </div>
-   <div>
-       <input type="text" id="text-input" placeholder="Question...">
-       <button id="insights-button">Get Insights</button>
-       <textarea id="generated-text" rows="10" cols="50" readonly></ textarea>
-   </div>
-     `;
-   class Widget extends HTMLElement {
-     constructor() {
-       super();
-       let shadowRoot = this.attachShadow({
-         mode: "open"
-       });
-       shadowRoot.appendChild(template.content.cloneNode(true));
-       this._props = {};
-     }
-     async connectedCallback() {
-       this.initMain();
-     }
-     async initMain() {
-       //Initialize parameters and set default as ""
-       const { user_id = "" } = this._props || {};
-       const { dashboard_name = "" } = this._props || {};
-       const { local_datetime = "" } = this._props || {};
-       const { authentication = "" } = this._props || {};
+  let template = document.createElement("template");
+  template.innerHTML = `
+<style>
+    :host {}
 
-       //Get UI elements
-       const generatedText = this.shadowRoot.getElementById("generated-text");
-       generatedText.value = "";
-       const generateButton = this.shadowRoot.getElementById("insights-button");
+    /* Style for the container */
+    div {
+        margin: 25px auto;
+        max-width: 80%;
+    }
 
-       // Handle button click
-       generateButton.addEventListener("click", async () => {
-         const promptInput = this.shadowRoot.getElementById("text-input");
-         const generatedText = this.shadowRoot.getElementById("generated-text");
-         generatedText.value = "We are processing your request";
-         const prompt = promptInput.value;
+    /* Style for the insights, title and list */
+    .insights {
+      display: block;
+      margin: 0 auto;
+      align-items: center;
+      padding: 20px;
+      box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+    }
 
-         // Define API endpoint metadata
-         const url = "https://hda-friendly-reporting.me.sap.corp/api/v1/llms/navigation";
-         const data = { user_prompt: prompt };
-         const options = {
-           method: "POST",
-           headers: {
-             'Content-Type': 'application/json',
-             'Authorization': 'Bearer sc9as24jlpp7994x' //to be set from the application side -------------------------
-           },
-           body: JSON.stringify(data)
-         };
+    .insights-title {
+      font-family: Arial, sans-serif;
+      font-size: 24px;
+      color: #333;
+      text-align: center;
+      margin-bottom: 20px;
+    }
 
-         // API call and output processing
-        await fetch(url, options)
-           .then((response) => {
-             const res = response;
-             if (res.ok) {
-               return res.json();
-             } else {
-               throw new Error('Network response failed.');
-             }
-           })
-           .then((data) => {
-             const output_guide = data.output_guide;
-             generatedText.value  = output_guide;
-           })
-           .catch((error) => console.error(`Fetch Error: ${error}`));
-       });
-     }
-     onCustomWidgetBeforeUpdate(changedProperties) {
-       this._props = {
-         ...this._props,
-         ...changedProperties
-       };
-     }
-     onCustomWidgetAfterUpdate(changedProperties) {
-       this.initMain();
-     }
-   }
-   customElements.define("insights-widget", Widget);
- })();
+    .insights-list {
+      list-style-type: disc;
+      margin-left: 20px;
+    }
+  
+    .insights-list li {
+      font-family: Arial, sans-serif;
+      font-size: 18px;
+      color: #eee;
+      margin-bottom: 10px;
+    }
+
+    /* Style for the input container */
+    .input-container {
+        display: block;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+    /* Style for the generated text area */
+    #generated-text {
+        padding: 10px 3%;
+        font-size: 16px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        width:94%;
+    }
+
+    /* Style for the button */
+    #insights-button {
+        padding: 10px;
+        font-size: 16px;
+        background-color: #3cb6a9;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        width: 25%;
+    }
+
+    /* Style for the input field */
+    #text-input {
+        padding: 10px;
+        font-size: 16px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        width: 70%;
+    }
+</style>
+
+  <div class="insights">
+    <h1 class="insights-title">Data Insights</h1>
+      <ul id="insightsList" class="insights-list">
+          
+      </ul>
+  </div>
+  <div class="input-container">
+      <input type="text" id="text-input" placeholder="Question...">
+      <button id="insights-button">Get Insights</button>
+  </div>
+  <textarea id="generated-text" rows="10" cols="50" readonly></ textarea>
+    `;
+  class Widget extends HTMLElement {
+    constructor() {
+      super();
+      let shadowRoot = this.attachShadow({
+        mode: "open"
+      });
+      shadowRoot.appendChild(template.content.cloneNode(true));
+      this._props = {};
+    }
+    async connectedCallback() {
+      this.initMain();
+    }
+    async initMain() {
+      //Initialize parameters and set default as ""
+      const { user_id = "" } = this._props || {};
+      const { dashboard_name = "" } = this._props || {};
+      const { local_datetime = "" } = this._props || {};
+      const { authentication = "" } = this._props || {};
+
+      //Get UI elements
+      const generatedText = this.shadowRoot.getElementById("generated-text");
+      generatedText.value = "";
+      const generateButton = this.shadowRoot.getElementById("insights-button");
+
+      //Initialise list elements
+      const insights = this.getRandomInsights();
+      const insightsList = this.shadowRoot.getElementById("insightsList");
+      insights.forEach(element => {
+        insightsList.innerHTML += ('<li>'+element+'</li>');
+      });
+
+      // Handle button click
+      generateButton.addEventListener("click", async () => {
+        const promptInput = this.shadowRoot.getElementById("text-input");
+        const generatedText = this.shadowRoot.getElementById("generated-text");
+        generatedText.value = "We are processing your request";
+        const prompt = promptInput.value;
+        
+        // Define API endpoint metadata
+        const url = "https://hda-friendly-reporting.me.sap.corp/api/v1/llms/navigation";
+        const data = { user_prompt: prompt };
+        const options = {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer sc9as24jlpp7994x' //to be set from the application side -------------------------
+          },
+          body: JSON.stringify(data)
+        };
+
+        // API call and output processing
+       await fetch(url, options)
+          .then((response) => {
+            const res = response;
+            if (res.ok) {
+              return res.json();
+            } else {
+              throw new Error('Network response failed.');
+            }
+          })
+          .then((data) => {
+            const output_guide = data.output_guide;
+            generatedText.value  = output_guide;
+          })
+          .catch((error) => console.error(`Fetch Error: ${error}`));
+      });
+    }
+    getRandomInsights(){
+      // replace with actual insights in production 
+      const jsonData = {
+        "insights": [
+            {
+                "id": 1,
+                "title": "Insight 1",
+                "content": "There was a spike in the runtime from the previous week. You might want to look into that"
+            },
+            {
+                "id": 2,
+                "title": "Insight 2",
+                "content": "The failures have stabilised over the past 3 weeks with 1 lesser failure per week."
+            },
+            {
+                "id": 3,
+                "title": "Insight 3",
+                "content": "The documentation has new sections detailing over the newly added charts."
+            },
+            {
+              "id": 4,
+              "title": "Insight 4",
+              "content": "Stages from release pipelines of Microservices ABC and XYZ have reduced runtimes."
+            },
+            {
+                "id": 5,
+                "title": "Insight 5",
+                "content": "The average runtime has reduced for microservices in ABX product."
+            },
+            {
+                "id": 6,
+                "title": "Insight 6",
+                "content": "The E2E runtime for Microservice ABX has reduced over the last 3 months."
+            },
+            {
+              "id": 7,
+              "title": "Insight 7",
+              "content": "There is higher adoption than the past months"
+            },
+            {
+                "id": 8,
+                "title": "Insight 8",
+                "content": "Change failure rates have dropped for bugs with regression label"
+            },
+            {
+                "id": 9,
+                "title": "Insight 9",
+                "content": "Time to reach development has increased over the past quarter"
+            }
+        ],
+        "user": {
+            "name": "I556644"
+        }
+      };
+      var randomIDs = this.getRandomIDs(3);
+      var randomInsights = [];
+      randomIDs.forEach(element => {
+        randomInsights.push(jsonData.insights[element].content);
+      });
+      return randomInsights;
+    }
+    getRandomIDs(length){
+      var arr = [];
+      while(arr.length < length){
+          var r = Math.floor(Math.random() * length);
+          if(arr.indexOf(r) === -1) arr.push(r);
+      }
+      return arr;
+    }
+    onCustomWidgetBeforeUpdate(changedProperties) {
+      this._props = {
+        ...this._props,
+        ...changedProperties
+      };
+    }
+    onCustomWidgetAfterUpdate(changedProperties) {
+      this.initMain();
+    }
+  }
+  customElements.define("external-friendly-reporting-insights", Widget);
+})();
