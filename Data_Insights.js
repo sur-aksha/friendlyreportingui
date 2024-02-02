620fd15df8ab3aed6021efafd32913aec20d153d
Suraksha Balachandra <96290932+sur-aksha@users.noreply.github.com>
2024-02-02 11:56:30 +0100

    bug fix

--- Data_Insights.js
+++ Data_Insights.js
@@ -253,7 +253,7 @@
         },
       };
 
-      fetch(apiUrl, requestOptions)
+      fetch(apiURL, requestOptions)
         .then((response) => {
           if (!response.ok) {
             throw new Error('Network response failed');
